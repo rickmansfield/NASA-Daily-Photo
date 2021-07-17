06e2b327b180189e10aba2d323d1da67b618189d
Rick <rmansfield@realpeoplesratings.com>
2021-07-17 15:31:57 -0500

    Updates

---
+++ precache-manifest.4dc4058940dd426bcdf481b8dec17045.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed2080b0241a4548753abe43794b9098",
+    "url": "/NASA-Daily-Photo/index.html"
+  },
+  {
+    "revision": "da218c38a97b5730a574",
+    "url": "/NASA-Daily-Photo/static/css/main.cc6235dc.chunk.css"
+  },
+  {
+    "revision": "5a8163012c4600788b28",
+    "url": "/NASA-Daily-Photo/static/js/2.457b739e.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/NASA-Daily-Photo/static/js/2.457b739e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da218c38a97b5730a574",
+    "url": "/NASA-Daily-Photo/static/js/main.b5e83241.chunk.js"
+  },
+  {
+    "revision": "8cb629077a59b52bc3a8",
+    "url": "/NASA-Daily-Photo/static/js/runtime-main.0a498b6c.js"
+  }
+]);
--- src/constants/key.js
+++
@@ -1,4 +0,0 @@
-export const BASE_URL = 'https://api.nasa.gov/planetary/apod?';
-export const API_KEY = 'api_key=lXBIKau7qHM9I492YNjveWfR5wg4S8ggzMhAhJUa';
-
-//https://api.nasa.gov/#apod for more details on the key. 
